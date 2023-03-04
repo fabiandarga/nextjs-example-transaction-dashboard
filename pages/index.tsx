5ba520e32245320bfb2bec35c58ed27f3c5e199b
Fabian Darga <kontakt@fabiandarga.de>
2023-03-04 19:52:24 +0100

    nextjs setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+function HomePage() {
+    return (
+        <div>
+            <p className={styles.paragraph}>Welcome to Next.js!</p>
+        </div>
+    )
+}
+
+export default HomePage
